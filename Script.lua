ca2291ee7fef06ff186a3357d479b6ac30c07b48
MrWhite8901 <mrwhite95935799@gmail.com>
2024-09-18 10:19:13 +0800

    Delete Script.lua

--- Script.lua
+++
@@ -1,2 +0,0 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Test", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
