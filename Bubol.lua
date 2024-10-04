bbd3fc2e1bad777c5f76e636b7067beed9aec457
MrWhite8901 <mrwhite95935799@gmail.com>
2024-10-04 22:02:55 +0800

    Save Bubol.lua.

--- Bubol.lua
+++ Bubol.lua
@@ -14,22 +14,22 @@ local Window = Rayfield:CreateWindow({
       Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
-   KeySystem = false, -- Set this to true to use our key system
+   KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
-      Title = "Untitled",
+      Title = "Bobul",
       Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided",
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      Note = "Copy and paste it on google",
+      FileName = "Key_System", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"Buderdene"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
 
 Rayfield:Notify({
    Title = "You executed the script",
-   Content = nil,
-   Duration = 5,
+   Content = "",
+   Duration = 3,
    Image = 13047715178,
    Actions = { -- Notification Buttons
       Ignore = {
@@ -42,8 +42,9 @@ Rayfield:Notify({
 })
 
 local MainTab = Window:CreateTab("Main", 4483362458) -- Title, Image
-local MainSection = MainTab:CreateSection("Section Example")
+local MainSection = MainTab:CreateSection("Main")
 
+-- Developers Section
 local MainSection = MainTab:CreateSection("For Developers")
 
 local Button = MainTab:CreateButton({
@@ -54,7 +55,7 @@ local Button = MainTab:CreateButton({
 })
 
 local Button = MainTab:CreateButton({
-   Name = "Open Console",
+   Name = "Console",
    Callback = function()
        -- This will open the developer console when the button is clicked
        game:GetService("StarterGui"):SetCore("DevConsoleVisible", true)
