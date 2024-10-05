14c6b51d079822e1b63efaae64705aefe83f9ed7
MrWhite8901 <mrwhite95935799@gmail.com>
2024-10-05 11:14:03 +0800

    Save Bubol.lua.

--- Bubol.lua
+++ Bubol.lua
@@ -21,8 +21,8 @@ local Window = Rayfield:CreateWindow({
       Note = "Copy and paste it on google",
       FileName = "Key_System", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Buderdene"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"https://pastebin.com/raw/9st6nCKW"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
 
